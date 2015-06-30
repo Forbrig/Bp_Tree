7fdd6f607068e122a4b882fb6d59c49774b821f8
Forbrig <vitorforbrig@gmail.com>
2015-06-30 20:17:15 -0300

    not working... yet... great changes i mean...

--- source/bp_node.c
+++ source/bp_node.c
@@ -5,11 +5,11 @@
 #define NODE_MINIMUM 2
 
 typedef struct bp_node {
-	boolean isLeaf, isRoot;
+	_Bool isLeaf, isRoot;
 	int size;
 	int key[NODE_LENGHT];//satellites if aren't leafs
 	//int pos[NODE_LENGHT +1]; //literaly position of the left/right node.
-	struct bp_node[]* pos[NODE_LENGHT +1];
+	struct bp_node* pos[NODE_LENGHT +1];
 	struct bp_node* father;
 	struct bp_node* previous;
 	struct bp_node* next;
--- source/bp_tree.c
+++ source/bp_tree.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include "bp_node.c"
 #include "bp_tree.h"
 
 //create a new empty node;
@@ -12,19 +13,21 @@ bp_node new_node () {
 	new->father = NULL;
 	new->previous = NULL;
 	new->next = NULL;
+	int i;
 	for (i = 0; i < (NODE_LENGHT +1); i++) {
 		new->pos[i] = NULL;
 	}
-	return new;
+	return* new;
 }
 
 //search the node that will receive the new key
 //recursion
 //means ok
-bp_node search_node(bp_node root, int key) {
+bp_node* search_node(bp_node* root, int key) {
 	if (root->isLeaf == false) {
+		int i;
 		for (i = 0; i < NODE_LENGHT && root->pos[i]; i++) {
-			if (key <= root->key[i]) {
+			if (key < root->key[i]) {
 				//left of the satellite
 				search_node (root->pos[i], key);
 			} else if (root->size == i + 1) { //i + 1 because size never is zero in this case
@@ -47,44 +50,109 @@ int mid_node( ) {
 	}
 }
 
-void split_node(bp_node x) {
-	int i, j;
-	bp_node* new = new_node();
-	for (j = 0, i = (mid_node() + 1); i < NODE_LENGHT; i++, j++) {
-		new->key[j] = x->key[i];
-	
+//update pointers o/
+void update_pointers(bp_node father, bp_node r, int index) {
+	father->pos[index+1] = r;
 }
 
+//all internal splits are caused by splits in the leaves
+//danger zone, hard work (needs understand)
+bp_node* split_node(bp_node* x) {
+	int i, j;
+	bp_node* new;
+	*new = new_node();
+	x->size = mid_node();
+	if (NODE_LENGHT % 2 == 0) {
+		new->size = mid_node();
+		for (j = 0, i = (mid_node() + 1); i < NODE_LENGHT; i++, j++) {
+			new->key[j] = x->key[i];
+		}
+	} else {
+		new->size = mid_node() + 1;
+		for (j = 0, i = mid_node(); i < NODE_LENGHT; i++, j++) {
+			new->key[j] = x->key[i];
+		}
+	}
+	return new;
+}
+
+bp_node* best_node_for_this_key(bp_node* l, bp_node* r, int key) {
+	if (l->key[l->size] < key) {
+		return* r;
+	} else {
+		return* l;
+	}
+}
 
 //pre insert a new key on a leaf (root can be a leaf!)
 //cases of insertion
-void insert_key(bp_node x, int key) {
-	bp_node* aux;
-	aux = search_node (root, key);
+void insert_key(bp_node root, int key) {
+	int index; //index of the new satellite, ifs needed
+	bp_node* aux, aux2;
+	aux = search_node(root, key);
 	//case 1: the leaf is not full
 	if (aux->size < NODE_LENGHT) {
 		insert_key2(x, key);
 	//case 2: the leaf is full
 	} else if (aux->size == NODE_LENGHT) {
+		//split!
+		aux2 = split_node(x);
+		//make sure thats (right/left) is the best place
+		index = insert_key2(best_node_for_this_key(aux, aux2, key), key);
+		//case 2.1: the node is root, depth is increased
+		if(aux->isRoot == true) {
+			bp_node* new;
+			*new = new_node();
+			new->isRoot == true;
+			insert_key(new, aux2->[0]);
+			new->pos[0] = aux;
+			update_pointers(new, aux2, 0); // same = new->pos[1] = aux2;
+			aux->isRoot = false;
+			aux2->isRoot = false;
+		//case 2.2: if the internal node dont needs to split
+		} else if (aux->father && aux->father->size < NODE_LENGHT) {
+			insert_key2(aux->father, aux2->key[0]);
+			update_pointers(aux->father, aux2, index);
+		//case 2.3: an internal node needs to split
+		} else if (aux->father && aux->father->size == NODE_LENGHT) {
+			aux3 = split_node(aux->father);
+			insert_key(aux->father, aux2->key[0]);
+			
+		}
 	}
 }
 
-//insert once and for all 
-void insert_key2(bp_node x, key) {
-	//redistribuir os elementos do nodo
-	
-	
-	
+//insert once and for all
+//and push "pointers" to forward 
+int insert_key2(bp_node x, key) {
+	int aux, flag = -1;
+	int i;
+	bp_node* n, n2;
+	//simple ordenation
+	for (i = 0; i < x->size - 1; i++) {
+		if (key < x->key[i]) {
+			aux = x->key[i];
+			x->key[i] = key;
+			key = aux;
+			//update pointers
+			if (flag == -1) {
+				flag = i;
+				n = pos[i+1];
+				for (int j = i + 1; j < x->size; j++) {
+					n2 = x->pos[j+1];
+					x->pos[j+1] = n;
+					n = n2;
+				}
+				x->pos[x->size] = n;
+			}
+				
+		} else { 
+			i++;
+		}
+	}
+	x->size++;
+	return flag; //return the index of the new element
 }
-
-
-
-
-
-
-
-
-
 
 
 
